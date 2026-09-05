d3e583f63b0f9c94d459c3110d460d8621b57f14
rupinder-tech <rupinderriar508@gmail.com>
2026-09-05 11:19:20 +0530

    Modified code

--- src/streams/FirstNonRepeatedChar.java
+++ src/streams/FirstNonRepeatedChar.java
@@ -1,28 +1,33 @@
 package streams;
 
-import java.util.LinkedHashMap;
-import java.util.stream.Collectors;
-
 public class FirstNonRepeatedChar {
 
     public static void findFirstNonRepeatedChar(String str){
+        // str.chars().
+        // mapToObj(c -> (char)c).
+        // collect(Collectors.groupingBy(
+        //     ch -> ch,
+        //     () -> new LinkedHashMap<>(),
+        //     Collectors.counting()
+        // )).
+        // entrySet().
+        // stream().
+        // filter(m -> m.getValue() == 1).
+        // map(m -> m.getKey()).findFirst().ifPresentOrElse(
+        //     c -> System.out.println(c), 
+        //     () -> System.out.println("not found"));
+
         str.chars().
         mapToObj(c -> (char)c).
-        collect(Collectors.groupingBy(
-            ch -> ch,
-            () -> new LinkedHashMap<>(),
-            Collectors.counting()
-        )).
-        entrySet().
-        stream().
-        filter(m -> m.getValue() == 1).
-        map(m -> m.getKey()).findFirst().ifPresentOrElse(
+        filter(c -> str.indexOf(c) == str.lastIndexOf(c)).
+        findFirst().ifPresentOrElse(
             c -> System.out.println(c), 
-            () -> System.out.println("not found"));
+            () -> System.out.println("not found")
+        );
     }
 
     public static void main(String[] args) {
-        String str = "swwiiss";
+        String str = "swiss";
         findFirstNonRepeatedChar(str);
     }
 }
